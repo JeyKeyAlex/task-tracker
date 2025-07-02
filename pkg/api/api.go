92af705f124f68875002a69119b2c8587b189266
Evgeniy Krasnikov <evgenykrasnikov01@gmail.com>
2025-07-02 17:21:49 +0300

    add_search_task_opportunity

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -7,7 +7,7 @@ import (
 func Init(r chi.Router) {
 	r.Get("/api/nextdate", getNextDayHandler)
 	r.With(auth).Post("/api/task", addTaskHandler)
-	r.Get("/api/tasks", getTaskListHandler)
+	r.With(auth).Get("/api/tasks", getTaskListHandler)
 	r.With(auth).Get("/api/task", getTaskByIdHandler)
 	r.With(auth).Put("/api/task", updateTaskHandler)
 	r.With(auth).Delete("/api/task", deleteTaskHandler)
